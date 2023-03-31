26bba0d646d42e22c27f1f3e8ea9ca04b9e6f73c
IUT GEII Toulon Table 6 <rct@univ-tln.fr>
2023-03-31 14:56:15 +0200

    31032023

--- C#/RobotInterface/MainWindow.xaml.cs
+++ C#/RobotInterface/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace RobotInterface
 {
@@ -23,6 +24,9 @@ namespace RobotInterface
     {
 
         ReliableSerialPort serialPort1;
+        DispatcherTimer timerAffichage = new DispatcherTimer();
+        string receivedText = "";
+
 
         public MainWindow()
         {
@@ -32,15 +36,32 @@ namespace RobotInterface
             //serialPort1.DataReceived += SerialPort1_DataReceived;
             serialPort1.Open();
 
+            timerAffichage.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timerAffichage.Tick += TimerAffichage_Tick;
+            timerAffichage.Start();
+
+
         }
 
-        //private void SerialPort1_DataReceived(object sender, DataReceivedArgs e)
-        //{}
+        private void TimerAffichage_Tick(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            if (receivedText != "")
+                textboxRéception.Text += receivedText;
+                receivedText = "";
 
+        }
+
+        //private void SerialPort1_DataReceived(object sender, DataReceivedArgs e){}
         private void SerialPort1_OnDataReceivedEvent(object sender, DataReceivedArgs e)
         {
-            textboxRéception.Text += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
+            //textboxRéception.Text += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
+            receivedText += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
         }
+
+
+
+
 
         bool toggle = false;
 
@@ -58,6 +79,27 @@ namespace RobotInterface
 
             SendMessage();
         }
+
+        private void buttonSupprimer_Clear(object sender, RoutedEventArgs e)
+        {
+
+            if (toggle == true)
+
+                buttonSupprimer.Background = Brushes.RoyalBlue;
+            else
+                buttonSupprimer.Background = Brushes.Gold;
+
+            toggle = !toggle;
+
+            ClearMessage();
+        }
+
+        void ClearMessage()
+        {
+            if (textboxRéception.Text != "")
+                textboxRéception.Text = "";
+        }
+
         void SendMessage()
         {
             //string messageEmis = textboxEmission.Text;
